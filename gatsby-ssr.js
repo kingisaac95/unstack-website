b949f9c8b9f8f96e7884b3eeb2b220d1a1a4b244
Kingdom Orjiewuru <kingdom.orjiewuru@andela.com>
2020-02-03 23:41:29 +0100

    feat: add event prime script to head

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,7 +1,16 @@
+const React = require("react")
+exports.onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <script
+      key="1"
+      type="text/javascript"
+      src="https://eventprime.co/js/widget.js"
+    />,
+  ])
+}
+
 /**
  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
  *
  * See: https://www.gatsbyjs.org/docs/ssr-apis/
  */
-
-// You can delete this file if you're not using it
