68f64c6d4f6db1fe5be404cf20e8a1d35a73b914
Orjiewuru Kingdom <kingdom.orjiewuru@gmail.com>
2020-02-10 13:53:07 +0100

    feat: pwa support

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,18 +18,16 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `unStack App`,
+        short_name: `unStack`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        background_color: `#00529C`,
+        theme_color: `#00529C`,
+        display: `standalone`,
+        icon: `src/images/favicon.png`,
       },
     },
     `gatsby-plugin-postcss`,
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
